20a4250d3820e2199095089897bca7a45c0bf322
Mike-OxHuge <mikelitoris34@icloud.com>
2021-08-24 16:33:48 +0200

    adds auth + admin auth; assign roles +delete user (admin only)

--- src/services/routes/users/index.js
+++ src/services/routes/users/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import UserModel from "./schema.js";
+import { basicAuthMiddleware, adminOnly } from "../../auth/index.js";
 
 const router = express.Router();
 
 router
   .route("/")
   // get all users
-  .get(async (req, res, next) => {
+  .get(basicAuthMiddleware, async (req, res, next) => {
     try {
       const users = await UserModel.find({});
       res.send(users);
@@ -24,6 +25,28 @@ router
     } catch (error) {
       console.log(error);
     }
+  })
+
+  //delete user, admin only
+  .delete(basicAuthMiddleware, adminOnly, async (req, res, next) => {
+    await UserModel.findOneAndDelete({ _id: req.body.id });
+    res.send("user has been deleted");
+  })
+
+  // change role, admin only
+  .put(basicAuthMiddleware, adminOnly, async (req, res, next) => {
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { email: req.body.email },
+      {
+        $set: { role: req.body.role },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    console.log(req.body);
+    res.send(updatedUser);
   });
 
 export default router;
