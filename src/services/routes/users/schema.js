bb4741e01cc9a1ccd022d8135a5999b55f208a4b
Mike-OxHuge <mikelitoris34@icloud.com>
2021-08-24 15:54:16 +0200

    hashing password

--- src/services/routes/users/schema.js
+++ src/services/routes/users/schema.js
@@ -16,4 +16,50 @@ const UserSchema = new Schema({
   },
 });
 
+UserSchema.pre("save", async function (next) {
+  // BEFORE(pre) saving new user in db, hash the password
+  const newUser = this; // apparently this is the user object
+
+  const plainPW = newUser.password;
+
+  if (newUser.isModified("password")) {
+    newUser.password = await bcrypt.hash(plainPW, 10);
+  }
+
+  next();
+});
+
+UserSchema.methods.toJSON = function () {
+  // toJSON is called every time express does a res.send
+
+  const userDocument = this;
+
+  const userObject = userDocument.toObject();
+
+  // delete userObject.password;
+  // keep it for now, cos I wanna see if it's hashed
+
+  delete userObject.__v;
+
+  return userObject;
+};
+
+UserSchema.statics.checkCredentials = async function (email, plainPW) {
+  // 1. find user in db by email
+
+  const user = await this.findOne({ email });
+
+  if (user) {
+    // 2. if user is found we need to compare plainPW with hashed PW
+    const isMatch = await bcrypt.compare(plainPW, user.password);
+
+    // 3. return a meaningful response
+
+    if (isMatch) return user;
+    else return null;
+  } else {
+    return null;
+  }
+};
+
 export default model("User", UserSchema);
